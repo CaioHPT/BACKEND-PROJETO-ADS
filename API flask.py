728e07b9dfb7db8d81b3759254555cc5aeda6fcc
caioteixeira <caio.h.teixeira@gmail.com>
2024-05-15 22:44:33 -0300

    Criado udpate

--- API flask.py
+++ API flask.py
@@ -5,26 +5,12 @@ import bd
 app = Flask(__name__)
 CORS(app)
 
-viagens = [
-    {"id":1, "local": "Destino 1"},
-    {"id":2, "local": "Destino 2"},
-    {"id":3, "local": "Destino 3"}
-]
-
 #Procurar todas as viagens
 @app.route('/viagens', methods=['GET'])
 def get_viagens():
     viagens = bd.session.query(bd.Viagens).order_by(bd.Viagens.id)
     viagens_json = [viagem.to_json() for viagem in viagens]
     return Response(json.dumps(viagens_json), status=200, mimetype="application/json")
-
-#Procurar uma viagem especifica por ID
-@app.route('/viagens/<int:destino_id>', methods=['GET'])
-def get_viagem(destino_id):
-    for viagem in viagens:
-        if viagem['id']==destino_id:
-            return viagem
-    return {'error':'Local não encontrado'}
 
 #Criando um cadastro de viagem
 @app.route('/viagens', methods=['POST'])
@@ -45,13 +31,24 @@ def create_viagem():
     
 
 #Atualizando uma viagem
-@app.route('/viagens/<int:destino_id>', methods=['GET'])
+@app.route('/viagens/<int:destino_id>', methods=['PUT'])
 def update_viagem(destino_id):
-    for viagem in viagens:
-        if viagem['id']==destino_id:
-            viagem['local']=request.json['local']
-            return viagem
-        return{'error':'Local não encontrado'}
+    body = request.get_json()
+    try:
+        viagem = bd.session.query(bd.Viagens).filter_by(id = destino_id).first();
+        viagem.origem = body['origem']
+        viagem.destino = body['destino']
+        viagem.valor = body['valor']
+        viagem.urlfoto = body['urlFoto']
+
+        bd.session.add(viagem)
+        bd.session.commit()
+
+        return Response(json.dumps(viagem.to_json()), status=200, mimetype="application/json")
+    except Exception as e:
+        print("Erro", e)
+        return Response(status=400)   
+
     
 #Deletando uma viagem
 @app.route('/viagens/<int:destino_id>', methods=['DELETE'])
